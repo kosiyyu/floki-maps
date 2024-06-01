a44b4427046a4243c5b33f61cf9c7a0e5f8f3161
Karol Koś <kosiyyu@gmail.com>
2024-06-01 17:06:33 +0200

    Add core maps functianality

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import 'leaflet/dist/leaflet.css';
+import 'vue-map-ui/dist/normalize.css';
+import 'vue-map-ui/dist/style.css';
+import 'vue-map-ui/dist/theme-all.css';
+
 createApp(App).mount("#app");
